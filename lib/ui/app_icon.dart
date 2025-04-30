74fef2dd69b82abcaac3b89a95ae8443782e420d
KJM-BlueVisor <kjm@bluevisor.kr>
2025-04-30 12:51:09 +0900

    feature: AppIcon 디자인 시작

--- lib/ui/app_icon.dart
+++ lib/ui/app_icon.dart
@@ -1,28 +1,41 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AppIconWidget extends StatelessWidget {
   final String iconName;
-  final Icon icon;
+  final String imagePath;
+  final VoidCallback onClick;
 
   const AppIconWidget({
     required this.iconName,
-    required this.icon,
+    required this.imagePath,
+    required this.onClick,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16.0),
+    return Padding(
+      padding: EdgeInsets.only(bottom: 48),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+
+        children: [
+          Image.asset(
+            imagePath,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
           ),
-        )
-      ],
+          Text(
+            iconName,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            )
+          ),
+        ],
+      ),
     );
   }
 }
